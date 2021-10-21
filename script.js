b19b1b600610672626cde1095cff80d8a16541cf
aakovalenko <kaa89pl@gmail.com>
2021-10-21 09:15:25 +0300

    task 2.4

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ if (age >= 14 && age <= 90) {
 } else {
     console.log ("неверно");
 }
+/* 2.4 */
+const number = +prompt('Введите число между 0 и 3', '');
+switch (number) {
+    case 0 : 
+    alert('Вы ввели число 0');
+    break;
+    case 1 :
+        alert('Вы ввели число 1');
+        break;
+        case 2 :
+        case 3 :
+            alert('Вы ввели число 2, а может и 3');
+            break;
+        default : 
+        alert('Вы ввели другое число');
+        break;
+}
