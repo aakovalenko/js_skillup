8b88442b47d1ca1da4fb087c2aa23d689e3bef2b
aakovalenko <kaa89pl@gmail.com>
2021-10-21 09:00:00 +0300

    task 2.1

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ name = "Джон";
 admin = name;
 console.log(admin);
 
+/* Task2 */
+/* 2.1 */
+let userNumber = prompt("Ведите число меньше 10", 0);
+console.log(userNumber);
+if (userNumber < 10) {
+    alert("верно");
+} else {
+    alert("неверно");
+}
