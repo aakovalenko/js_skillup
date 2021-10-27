4ca8a827b5694b25077ba10d14c17f496a272964
aakovalenko <kaa89pl@gmail.com>
2021-10-27 11:53:54 +0300

    task 1

--- script.js
+++ script.js
@@ -1,85 +1,10 @@
-/* === Task1 === */
-let admin, name;
-name = "Джон";
-admin = name;
-console.log(admin);
+/* Task 1 */
+let someArr = [1, 2, 3, 4, 5];
 
-/* === Task2 === */
-/* 2.1 */
-let userNumber = prompt("Ведите число меньше 10", 0);
-console.log(userNumber);
-if (userNumber < 10) {
-    alert("верно");
-} else {
-    alert("неверно");
-}
-/* 2.2 */
-let message, login;
-
-switch (login) {
-    case  'Сотрудник' :
-        message = 'Привет';
-        console.log(message);
-        break;
-    case 'Директор' :
-        message = 'Здравствуйте';
-        console.log(message);
-        break;
-    default :
-        message = 'Нет логина';
-        console.log(message);
-        break;
-}
-/* 2.3 */
-let age;
-age =45;
-if (age >= 14 && age <= 90) {
-    console.log ("верно");
-} else {
-    console.log ("неверно");
-}
-/* 2.4 */
-const number = +prompt('Введите число между 0 и 3', '');
-switch (number) {
-    case 0 : 
-    alert('Вы ввели число 0');
-    break;
-    case 1 :
-        alert('Вы ввели число 1');
-        break;
-        case 2 :
-        case 3 :
-            alert('Вы ввели число 2, а может и 3');
-            break;
-        default : 
-        alert('Вы ввели другое число');
-        break;
+for (const oneArr of someArr) {
+    console.log(oneArr);
 }
 
-/* === Task3 === */
-/* 3.1 */
-let startNum = 2;
-for(startNum; startNum  <= 100; startNum++) {
-    if (startNum % 2 == 0) {
-        console.log(startNum);
-    }
+for (let i = 0; i <  someArr.length; i++) {
+    console.log(`Element - ${someArr[i]}`);
 }
-/* 3.2 */
-
-let userNumberPrompt; 
-do {
-    userNumberPrompt = prompt("Введите число больше 100",0);
-} while (userNumberPrompt <= 100 && userNumberPrompt);
-
-/* 3.3 */
-
-let n = 20;
-
-nextIterration: 
-for (let z = 2; z <= n; z++) {
-    for (let b = 2; b < z; b++) {
-        if (z % b == 0) continue nextIterration;
-    }
-    console.log(z);
-}
-
