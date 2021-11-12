b6e4a7e039c23f7575e8699490e474dc47672e97
aakovalenko <kaa89pl@gmail.com>
2021-11-12 21:55:27 +0200

    task 2

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 /* Task 1 */
 
-/*
-Реализуйте класс Worker (Работник),
- который будет иметь следующие свойства:
- name (имя),
-  surname (фамилия),
-   rate (ставка за день работы),
-    days (количество отработанных дней).
-     Также класс должен иметь метод getSalary(),
-      который будет выводить зарплату работника.
-       Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
- С помощью нашего класса создайте двух рабочих и найдите сумму их зарплат.
-*/
-
 class Worker {
     constructor(name, surname, rate, days) {
         this.name = name;
@@ -32,3 +19,51 @@ let SumOfSalary2Worker = worker1.getSalary() + worker2.getSalary();
 
  console.log(`Sum of salary worker - ${SumOfSalary2Worker}`);
 
+/* Task 2 */
+
+/*
+Реализуйте класс MyString,
+ который будет иметь следующие методы: метод reverse(),
+  который параметром принимает строку,
+   а возвращает ее в перевернутом виде,
+    метод ucFirst(),
+    который параметром принимает строку, а возвращает эту же строку,
+     сделав ее первую букву заглавной 
+     и метод
+      ucWords,
+ который принимает строку и делает заглавной первую букву каждого слова этой строки.
+ */
+class MyString {
+
+    // constructor(mystr) {
+    //     this.mystr = mystr;
+    // }
+
+    reverse(str) {
+        return str.split('').reverse().join('');
+    }
+
+    ucFirst(str) {
+        return str[0].toUpperCase() + str.slice(1);
+    }
+    ucWords(str) {
+        if (!str) return str; 
+
+        let newTempStrArray = str.split(' ');
+        let newStr = [];
+
+        for (let i of newTempStrArray) {
+            newStr.push(this.ucFirst(i))
+        } 
+
+        return newStr.join(' ');
+    }
+}
+
+let my_string = new MyString();
+
+console.log(my_string.reverse("Hello World"));
+
+console.log(my_string.ucFirst("kokonut"));
+
+console.log(my_string.ucWords("kokonut bibonat serede pepepe"));
